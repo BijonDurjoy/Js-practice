333d389431ed5e314bf261ee901abc5f9fc82f1b
BIJON SAHA <bijon176193@gmail.com>
2024-03-20 19:34:53 +0600

    Timer added

--- Bubble Game/script.js
+++ Bubble Game/script.js
@@ -1,7 +1,26 @@
-let bubbles="";
+let timeLeft = 10;
+function resetGame() {
+    let bubbles = "";
 
-for(let i=0; i<176; i++){
-    bubbles += '<div class="bubble">1</div>'
+    for (let i = 0; i < 176; i++) {
+        let value = Math.floor(Math.random() * 10);
+        bubbles += `<div class="bubble">${value}</div>`
+    }
+    let mainGameCanvas = document.querySelector(".main-canvas");
+    mainGameCanvas.innerHTML = bubbles;
 }
-let mainGameCanvas = document.querySelector(".main-canvas");
-mainGameCanvas.innerHTML = bubbles;
+
+function addTimer() {
+    let timer = setInterval(() => {
+        if (timeLeft === 0) {
+            clearInterval(timer);
+            document.querySelector(".main-canvas").innerHTML = "Game over"
+        }
+        else {
+            timeLeft--;
+            document.querySelector("#current-time").innerHTML = timeLeft;
+        }
+    }, 1000);
+}
+resetGame();
+addTimer();
