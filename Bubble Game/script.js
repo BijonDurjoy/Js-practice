722929c3384a1c74b15d7c83cdd1c6fa75dd5b91
BIJON SAHA <bijon176193@gmail.com>
2024-03-21 12:26:17 +0600

    main game  functionality done

--- Bubble Game/script.js
+++ Bubble Game/script.js
@@ -1,4 +1,6 @@
 let timeLeft = 10;
+let curntTarget = 0;
+let score = 0;
 function resetGame() {
     let bubbles = "";
 
@@ -6,6 +8,8 @@ function resetGame() {
         let value = Math.floor(Math.random() * 10);
         bubbles += `<div class="bubble">${value}</div>`
     }
+    curntTarget = Math.floor(Math.random()*10);
+    document.querySelector("#target-value").innerHTML = curntTarget
     let mainGameCanvas = document.querySelector(".main-canvas");
     mainGameCanvas.innerHTML = bubbles;
 }
@@ -22,5 +26,18 @@ function addTimer() {
         }
     }, 1000);
 }
+
+document.querySelector(".main-canvas").addEventListener("click",(x) =>{
+    let num = Number(x.target.innerHTML);
+    if(num === curntTarget){
+        resetGame();
+        score += timeLeft;
+        timeLeft = 10;
+        document.querySelector("#score").innerHTML = score;
+    }
+    else{
+        timeLeft = 0;
+    }
+})
 resetGame();
 addTimer();
